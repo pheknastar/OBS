ce0d21dc8bd0e2d6d86fed6e8d84d187f8d18a85
pheknastar <pushkark54@gmail.com>
2024-03-11 16:16:29 +0530

    commitDone1

--- src/test/java/customer/loaded.java
+++ src/test/java/customer/loaded.java
@@ -3,7 +3,6 @@ package customer;
 public class loaded {
 
 	public static void main(String[] args) {
-		System.out.println("hello123");
-		System.out.println("hello123");
+		System.out.println("hello1234");
 	}
 }
